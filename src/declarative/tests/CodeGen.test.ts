15a95de6cb523dba024d4d37fc205bf6d11ddcd0
Nathan Duke <naduke@microsoft.com>
2020-08-26 10:49:37 -0700

    Missed a type declaration

--- src/declarative/tests/CodeGen.test.ts
+++ src/declarative/tests/CodeGen.test.ts
@@ -6,7 +6,7 @@ import DocumentTranslator from '../translators/DocumentTranslator';
 describe('Code generation', () => {
   it.skip('can translate a valid XML document into a text file', async () => {
     return readFile('./src/declarative/tests/TestValid.xml').then(xmlStr => {
-      const xml = parseXml(xmlStr, { noblanks: true });
+      const xml: Document = parseXml(xmlStr, { noblanks: true });
       const documentTranslator: DocumentTranslator = new DocumentTranslator('babylonjs', '../../index');
       const fileOutput: string = documentTranslator.translate(xml);
       return writeFile('./src/declarative/tests/Valid.ts', fileOutput);
